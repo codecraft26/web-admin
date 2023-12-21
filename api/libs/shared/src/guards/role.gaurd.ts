03fcdbf53ec9c4ad583ad67d6237f8241b60d4b8
codecraft26 <gaman0221@gmial.com>
2023-12-21 13:53:07 +0530

    Update package name and password in package.json and postgresdb.module.ts

--- api/libs/shared/src/guards/role.gaurd.ts
+++ api/libs/shared/src/guards/role.gaurd.ts
@@ -1,24 +1,15 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import { CanActivate, ExecutionContext } from '@nestjs/common';
 
-export class RoleGaurd implements CanActivate{
-        private rolePassed:String
+export class RoleGaurd implements CanActivate {
+  private rolePassed: string;
 
-
-        constructor(private readonly allowedRoles:String[]){
-            
-        }
-    canActivate(context: ExecutionContext)  {
-
-       const {user}=context.switchToHttp().getRequest()
-       if(!user){
-              return false
-
-       }
-       
-       return this.allowedRoles.includes(user.role)
-
-
-        
+  constructor(private readonly allowedRoles: string[]) {}
+  canActivate(context: ExecutionContext) {
+    const { user } = context.switchToHttp().getRequest();
+    if (!user) {
+      return false;
     }
 
+    return this.allowedRoles.includes(user.role);
+  }
 }
